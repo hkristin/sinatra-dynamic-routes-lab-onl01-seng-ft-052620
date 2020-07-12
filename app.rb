e54da64cfe2b1d8dd1da526ce9efa6e494afd2e3
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-12 01:44:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do
-    @name = params[:name]
-  "${@name.reverse}"
+    params[:name].reverse
 end
 
 end
