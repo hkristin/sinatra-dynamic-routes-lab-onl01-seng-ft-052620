2bd84d15d738c1b97c203632dd5effec749abf74
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-12 02:22:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,8 +15,8 @@ class App < Sinatra::Base
       answer += params[:phrase]
       end
     answer
-  end
-end
+    end
+    
   get '/say/:word1/:word2/:word3 /:word4/:word5' do
     "#{params[:word1]}#{params[:word2]}#{params[:word3]}#{params[:word4]}#{params[:word5]}."
     end
