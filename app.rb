c9bc08b4bc58590b7e91f67dc79d7a243b91b6c1
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-12 02:36:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,6 +22,9 @@ class App < Sinatra::Base
     end
 
   get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    
   when 'add' 
     answer = (number1 + number2).to_s
   when 'subtract' 
