d158648ab92465257d293d7c86b6aa4554efd0e6
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-12 02:22:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,12 +13,12 @@ class App < Sinatra::Base
     answer = ''
     params[:number].to_i.times do
       answer += params[:phrase]
-    end
+      end
     answer
   end
 end
   get '/say/:word1/:word2/:word3 /:word4/:word5' do
     "#{params[:word1]}#{params[:word2]}#{params[:word3]}#{params[:word4]}#{params[:word5]}."
+    end
   end
-end
 end
